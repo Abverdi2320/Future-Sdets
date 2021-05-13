eca0916f82a9d64b6f784c44fe219dce35677c1d
Abbas Verdiyev <Abverdi2320@gmail.com>
2021-05-12 21:56:53 -0400

    Abbas-Selenium-Task -Facebook Task4

---
+++ src/test/java/day3_review_parctices/FacebookTask4_ValueVerification.java
@@ -0,0 +1,32 @@
+package day3_review_parctices;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookTask4_ValueVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://facebook.com");
+
+
+        //locating the "create a page" link
+        WebElement createPageLin = driver.findElement(By.linkText("Create a Page"));
+
+
+        String expectedInHref = "registration_form";
+        String actualHrefValue = createPageLin.getAttribute("href");
+
+        System.out.println("actualHrefValue = " + actualHrefValue);
+
+        if (actualHrefValue.contains(expectedInHref)){
+            System.out.println("HREF value verification Passed!");
+        }else{
+            System.out.println("HREF value Failed!!");
+        }
+
+    }
+}
