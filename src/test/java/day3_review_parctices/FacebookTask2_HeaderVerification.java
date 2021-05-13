3ee6aad9a4e142f89898755a6207c053b536cc51
Abbas Verdiyev <Abverdi2320@gmail.com>
2021-05-12 21:38:04 -0400

    Abbas-Selenium-Task -Facebook Task2

---
+++ src/test/java/day3_review_parctices/FacebookTask2_HeaderVerification.java
@@ -0,0 +1,27 @@
+package day3_review_parctices;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookTask2_HeaderVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://facebook.com");
+        WebElement header = driver.findElement(By.className("_8eso"));
+
+        String expectedHeader = "Connect with friends and the world around you on Facebook.";
+        String actualHeader = header.getText();
+
+        if (actualHeader.equals(expectedHeader)){
+            System.out.println("Header Verification Passed!");
+        }else{
+            System.out.println("Header Verification Failed!!");
+        }
+
+
+    }
+}
