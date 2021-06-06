3c26fad61e575dc6bab6cd98d47e80203dada2f2
Abbas Verdiyev <Abverdi2320@gmail.com>
2021-06-06 16:34:41 -0400

    PROPERTIES  (CONFIGURATION.PROPERTIES)

---
+++ src/test/java/day9_proproties_browser_utils/ReadingProperties.java
@@ -0,0 +1,30 @@
+package day9_proproties_browser_utils;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void readingFromPropertiesFile() throws IOException {
+        //#1- Create object of Properties class (Coming from Java library)
+
+        Properties properties = new Properties();
+
+        //#2- Open the file using FileInputStream
+        //We are trying to open a file, so we need to pass the path.
+        FileInputStream file = new FileInputStream("configuration.properties");
+
+        //#3- load the properties object with our file
+        properties.load(file);
+
+        //reading from configuration.properties
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"env\") = " + properties.getProperty("env"));
+        System.out.println("properties.getProperty(\"username\") = " + properties.getProperty("username"));
+
+    }
+}
